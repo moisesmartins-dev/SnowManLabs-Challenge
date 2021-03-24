b829db54e9becad1a62394b36a66da94508e1592
Moises Martins <moisesmartins2584@gmail.com>
2021-03-23 23:27:17 -0400

    created card

--- lib/app/ui/android/home_page/home_page.dart
+++ lib/app/ui/android/home_page/home_page.dart
@@ -4,9 +4,9 @@ import 'package:get/get.dart';
 //Color(0xff10159A)
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
+    // deixar transparente cor
     final theme = Theme.of(context).copyWith(dividerColor: Colors.transparent);
 
     return Scaffold(
@@ -22,43 +22,54 @@ class HomePage extends StatelessWidget {
         backgroundColor: Color(0xff10159A),
       ),
       body: Column(
-        children: [
-          Card(
-            elevation: 0.5,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            margin: EdgeInsets.all(20),
-            child: Padding(
-              padding: EdgeInsets.all(0),
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              elevation: 2.0,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
               child: Container(
                 decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10)
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.green,
                 ),
-                child: Theme(
-                  data: theme,
-                  child: ExpansionTile(
-                    childrenPadding: EdgeInsets.only(left: 5, bottom: 15),
-                    title: Text("Em qual data foi fundada a Snowman Labs?",
-                        style: TextStyle(
-                            color: Color(0xff10159A),
-                            fontSize: 16.0,
-                            fontWeight: FontWeight.bold)),
-                    trailing: Icon(Icons.arrow_drop_down),
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.all(10),
-                        child: Row(
-                          children: [
-                            Expanded(
-                                child: Text(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 3),
+                  child: Container(
+                    padding: EdgeInsets.only(left: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(9),
+                      color: Colors.white,
+                    ),
+                    child: Theme(
+                      data: theme,
+                      child: ExpansionTile(
+                        childrenPadding: EdgeInsets.only(left: 5, bottom: 15),
+                        title: Text("Em qual data foi fundada a Snowman Labs?",
+                            style: TextStyle(
+                                color: Color(0xff10159A),
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold)),
+                        trailing: Icon(Icons.arrow_drop_down),
+                        children: <Widget>[
+                          Padding(
+                            padding: new EdgeInsets.symmetric(
+                                horizontal: 10.0, vertical: 6.0),
+                            child: Row(
+                              children: [
+                                Expanded(
+                                    child: Text(
                                   "A Snowman Labs foi fundada no dia 9 de agosto de 2012.",
                                   style: TextStyle(color: Colors.grey[600]),
                                 ))
-                          ],
-                        ),
-                      )
-                    ],
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),

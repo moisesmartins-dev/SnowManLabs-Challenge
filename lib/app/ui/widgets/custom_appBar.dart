9ca04934f88cbf99a45ee1a2c1411079a564a4c3
Moises Martins <moisesmartins2584@gmail.com>
2021-03-25 03:10:37 -0400

    created search in app bar
    //TODO

---
+++ lib/app/ui/widgets/custom_appBar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+//TODO Text("Perguntas Frequentes") IN APPBAR
+
+class CustomAppBarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff10159A),
+        title: Container(
+          padding: EdgeInsets.only(top: 5),
+          child: TextFormField(
+            autofocus: true,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: "Procurar Pergunta",
+              hintStyle: TextStyle(color: Colors.grey),
+              prefixIcon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+              suffixIcon: IconButton(
+                icon: Row(
+                  children: [
+                    Icon(Icons.clear),
+                  ],
+                ),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
